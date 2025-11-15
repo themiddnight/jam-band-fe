2800521cd001261f5002227628d3ba706e4c3212
Pathompong Thitithan <the.midnight.k.0173@gmail.com>
2025-11-15 12:10:51 +0700

    drlete unused files

--- src/features/daw/components/multitrack/constants.ts
+++
@@ -1,5 +0,0 @@
-export const TRACK_HEADER_WIDTH = 260;
-export const TRACK_HEIGHT = 80;
-export const PIXELS_PER_BEAT = 80;
-export const RULER_HEIGHT = 36;
-
--- src/features/daw/components/pianoroll/constants.ts
+++
@@ -1,8 +0,0 @@
-export const KEYBOARD_WIDTH = 80;
-export const NOTE_HEIGHT = 16;
-export const RULER_HEIGHT = 32;
-export const SUSTAIN_LANE_HEIGHT = 48;
-export const TOTAL_KEYS = 88;
-export const LOWEST_MIDI = 21; // A0
-export const HIGHEST_MIDI = LOWEST_MIDI + TOTAL_KEYS - 1;
-
